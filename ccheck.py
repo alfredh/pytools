a1db482c22a17ad055d42d3aee0086680c5074e5
Alfred E. Heggestad <alfredh@io.lan>
2009-08-13 09:54:58 +0200

    added check_whitespace()

--- ccheck.py
+++ ccheck.py
@@ -9,24 +9,113 @@
 #    published by the Free Software Foundation.
 #
 
+import sys, os, re
 import fileinput
 
-version = '0.2.0'
-
 # config
+version = '0.1.0'
+
+# global variables
+errors = 0
+empty_lines_count = 0
 
 
+#
+# helper functions
+#
+
+#
+# print an error message and increase error count
+#
+def error(msg):
+    global errors
+    print "%s:%d: %s" % (fileinput.filename(), fileinput.lineno(), msg)
+    errors += 1
+
+
+#
+# print statistics
+#
+def print_stats():
+    print "Statistics:"
+    print "~~~~~~~~~~~"
+    print "Number of files processed:    "
+#    foreach (@extension) {
+#      print " $_: $stats{$_} "
+#    }
+    print ""
+    print "Number of lines with errors:   %d" % errors
+    print ""
+
+
+#
 # functions checking code
+#
 
 
+#
+# check for strange white space
+#
+def check_whitespace(line):
+
+    # chomp
+    line = line.rstrip('\n')
+    line_len = len(line)
+
+#    print "scanning line {%d} [%s]" % (line_len, line)
+
+    # general trailing whitespace check
+    if re.search(' $', line):
+        error("has trailing space(s)")
+    if re.search('\t$', line):
+        error("has trailing tab(s)")
+
+    # make sure TAB is used for indentation
+    for n in range(4, 17, 4):
+        if re.match('^ {%d}\S+' % n, line):
+            error("starts with %d spaces, use tab instead" % n)
+
+    # check for empty lines count
+    global empty_lines_count
+    if 1 == fileinput.lineno():
+        empty_lines_count = 0
+    if line_len == 0:
+        empty_lines_count += 1
+    else:
+        empty_lines_count = 0
+    if empty_lines_count > 2:
+        error("should have maximum two empty lines (%d)" % empty_lines_count)
+        empty_lines_count = 0
+
+    return
+
+
+#
 # main program
+#
 
+
+#
 # map of extensions, and which checks to perform
+#
 
+
+#
 # scan all files of extensions .xyz
 # parse all files
+#
+  
 
-# print stats
+# check program arguments
+if len(sys.argv) > 0:
+    for line in fileinput.input():
+        check_whitespace(line)
+else:
+    # scan recurs
+    pass
 
-for line in fileinput.input():
-    print "== %s:%d: %s" % (fileinput.filename(), fileinput.filelineno(), line.rstrip())
+
+#
+# done - print stats
+#
+print_stats()
