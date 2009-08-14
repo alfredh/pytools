ccc243893956baaab788ac40d2a93c3baba91016
Alfred E. Heggestad <aeh@db.org>
2009-08-14 20:45:39 +0200

    convert functions to class+methods

--- ccheck.py
+++ ccheck.py
@@ -14,322 +14,295 @@
 #
 #
 # TODO:
-# - object oriented: make a class ccheck
+# - optimize regex functions
 #
 
 import sys, os, re, fnmatch
 
-# config
-version = '0.1.0'
-extensions = ['c', 'cpp', 'h', 'mk', 'm4']
-
-# global variables
-files = {}
-errors = 0
-empty_lines_count = 0
-cur_filename = ''
-cur_lineno = 0
+VERSION = '0.1.0'
 
 
-#
-# extension mapping:
-#
-#   C-files:        .c
-#   Header files:   .h  .hh
-#   C++ files:      .cpp .cc
-#   Makefiles:      Makefile, mk, mak
-#
-extmap = {
-    'c':    ['*.c'],
-    'h':    ['*.h'],
-    'cpp':  ['*.cpp', '*.cc'],
-    'mk':   ['*Makefile', '*.mk'],
-    'm4':   ['*.m4'],
-    }
+###
+### Class definition
+###
+
+class ccheck:
+
+    def __init__(self):
+        self.errors = 0
+        self.cur_filename = ''
+        self.cur_lineno = 0
+        self.empty_lines_count = 0
+        self.files = {}
+        self.extensions = ['c', 'cpp', 'h', 'mk', 'm4']
+        
+        # empty dict
+        for e in self.extensions:
+            self.files[e] = []
+
+        self.common_checks = [self.check_whitespace, self.check_termination,
+                              self.check_hex_lowercase, self.check_pre_incr,
+                              self.check_file_unix]
+        self.funcmap = {
+            'c':    [self.check_brackets, self.check_c_preprocessor],
+            'h':    [self.check_brackets],
+            'cpp':  [self.check_brackets],
+            'mk':   [],
+            'm4':   [self.check_brackets, self.check_c_comments],
+            }
+        self.extmap = {
+            'c':    ['*.c'],
+            'h':    ['*.h'],
+            'cpp':  ['*.cpp', '*.cc'],
+            'mk':   ['*Makefile', '*.mk'],
+            'm4':   ['*.m4'],
+            }
+        self.maxsize = {
+            'c':  (79, 3000),
+            'h':  (79, 1000),
+            'mk': (79, 1000),
+            'm4': (79, 3000),
+            }
 
 
-# Maximum size of files: X * Y
-maxsize = {
-    'c':  (79, 3000),
-    'h':  (79, 1000),
-    'mk': (79, 1000),
-    'm4': (79, 3000),
-    }
-
-
-#
-# print an error message and increase error count
-#
-def error(msg):
-    global errors
-    print >> sys.stderr, "%s:%d: %s" % (cur_filename, cur_lineno, msg)
-    errors += 1
-
-
-#
-# print statistics
-#
-def print_stats():
-    print "Statistics:"
-    print "~~~~~~~~~~~"
-    print "Number of files processed:   ",
-    for e in extensions:
-        print " %s: %d" % (e, len(files[e])),
-    print ""
-    print "Number of lines with errors:   %d" % errors
-    print ""
-
-
-#
-# functions checking code
-#
-
-
-#
-# check for strange white space
-#
-def check_whitespace(line, len):
-
-    if len == 0:
-        return
-
-    # general trailing whitespace check
-    if line[-1] == ' ':
-        error("has trailing space(s)")
-
-    if line[-1] == '\t':
-        error("has trailing tab(s)")
-
-    # make sure TAB is used for indentation
-    for n in range(4, 17, 4):
-        if len > n and line[0:n] == ' '*n and line[n] != ' ':
-            error("starts with %d spaces, use tab instead" % n)
-
-
-def check_whitespace2(line, len):
+    def __del__(self):
+        pass
     
-    # check for empty lines count
-    global empty_lines_count
-    if len == 0:
-        empty_lines_count += 1
-    else:
-        empty_lines_count = 0
-    if empty_lines_count > 2:
-        error("should have maximum two empty lines (%d)" % empty_lines_count)
-        empty_lines_count = 0
+
+    #
+    # print an error message and increase error count
+    #
+    def error(self, msg):
+        print >> sys.stderr, "%s:%d: %s" % \
+              (self.cur_filename, self.cur_lineno, msg)
+        self.errors += 1
 
 
-#
-# check for end of line termination issues
-#
-def check_termination(line, len):
-
-    if len < 2:
-        return
-
-    if line[-1] == ';':
-        if line[-2] == ' ':
-            error("has spaces before terminator")
-
-    if line[-2:] == ';;':
-        error("has double semicolon")
+    #
+    # print statistics
+    #
+    def print_stats(self):
+        print "Statistics:"
+        print "~~~~~~~~~~~"
+        print "Number of files processed:   ",
+        for e in self.extensions:
+            print " %s: %d" % (e, len(self.files[e])),
+        print ""
+        print "Number of lines with errors:   %d" % self.errors
+        print ""
 
 
-#
-# check for C++ comments
-#
-def check_c_preprocessor(line, len):
+    #
+    # check for strange white space
+    #
+    def check_whitespace(self, line, len):
 
-    index = line.find('//')
-    if index != -1:
-        if line[index-1] != ':':
-            error("C++ comment, use C comments /* ... */ instead")
+        if len == 0:
+            return
+
+        # general trailing whitespace check
+        if line[-1] == ' ':
+            self.error("has trailing space(s)")
+
+        if line[-1] == '\t':
+            self.error("has trailing tab(s)")
+
+        # make sure TAB is used for indentation
+        for n in range(4, 17, 4):
+            if len > n and line[0:n] == ' '*n and line[n] != ' ':
+                self.error("starts with %d spaces, use tab instead" % n)
+
+        # check for empty lines count
+        if len == 0:
+            self.empty_lines_count += 1
+        else:
+            self.empty_lines_count = 0
+        if self.empty_lines_count > 2:
+            error("should have maximum two empty lines (%d)" % \
+                  self.empty_lines_count)
+            self.empty_lines_count = 0
 
 
-#
-# check that C comments are not used
-#
-def check_c_comments(line, len):
+    #
+    # check for end of line termination issues
+    #
+    def check_termination(self, line, len):
 
-    if line.find('/*') != -1 or line.find('*/') != -1:
-        error("C comment, use Perl-style comments # ... instead");
+        if len < 2:
+            return
+
+        if line[-1] == ';':
+            if line[-2] == ' ':
+                self.error("has spaces before terminator")
+
+        if line[-2:] == ';;':
+            self.error("has double semicolon")
 
 
-#
-# check max line length and number of lines
-#
-# param 1 - Maximum chars per line
-# param 2 - Maximum lines per file
-# param 3 - Filename
-#
-def check_xy_max(line, line_len, max_x):
+    #
+    # check for C++ comments
+    #
+    def check_c_preprocessor(self, line, len):
 
-    # expand TAB to 8 spaces
-    l = len(line.expandtabs())
+        index = line.find('//')
+        if index != -1:
+            if line[index-1] != ':':
+                self.error("C++ comment, use C comments /* ... */ instead")
+
+
+    #
+    # check that C comments are not used
+    #
+    def check_c_comments(self, line, len):
+
+        if line.find('/*') != -1 or line.find('*/') != -1:
+            self.error("C comment, use Perl-style comments # ... instead");
+
+
+    #
+    # check max line length and number of lines
+    #
+    def check_xy_max(self, line, line_len, max_x):
+
+        # expand TAB to 8 spaces
+        l = len(line.expandtabs())
     
-    if l > max_x:
-	error("line is too wide (" + str(l) + " - max " + str(max_x) + ")");
+        if l > max_x:
+            self.error("line is too wide (" + str(l) + " - max " + str(max_x) + ")");
 
-#    TODO:
-#    if ($line > $max_y) {
-#      error("is too big ($lines lines - max $max_y)\n");
+        #    TODO:
+        #    if ($line > $max_y) {
+        #      error("is too big ($lines lines - max $max_y)\n");
 
 
-#
-# check that hexadecimal numbers are lowercase
-#
-def check_hex_lowercase(line, len):
+    #
+    # check that hexadecimal numbers are lowercase
+    #
+    def check_hex_lowercase(self, line, len):
 
-    m = re.search('0x([0-9A-Fa-f]+)', line)
-    if m:
-        a = m.group(1)
-        if re.search('[A-F]+', a):
-            error("0x%s should be lowercase" % a)
+        m = re.search('0x([0-9A-Fa-f]+)', line)
+        if m:
+            a = m.group(1)
+            if re.search('[A-F]+', a):
+                self.error("0x%s should be lowercase" % a)
         
 
-#
-# check for correct brackets usage in C/C++
-#
-# TODO: this is too slow, optimize
-#
-def check_brackets(line, len):
+    #
+    # check for correct brackets usage in C/C++
+    #
+    # TODO: this is too slow, optimize
+    #
+    def check_brackets(self, line, len):
 
-    operators = ["do", "if", "for", "while", "switch"]
+        operators = ["do", "if", "for", "while", "switch"]
 
-    m = re.search('\W*(\w+\W*)\(', line)
-    if m:
-        keyword = m.group(1)
+        m = re.search('\W*(\w+\W*)\(', line)
+        if m:
+            keyword = m.group(1)
 
-        if keyword.strip() in operators:
-            if not re.search('[ ]{1}', keyword):
-                error("no single space after operator '%s()'" % keyword)
+            if keyword.strip() in operators:
+                if not re.search('[ ]{1}', keyword):
+                    self.error("no single space after operator '%s()'" % keyword)
 
-    # check that else statements do not have preceeding
-    # end-bracket on the same line
-    if re.search('\s*\}\s*else', line):
-        error("else: ending if bracket should be on previous line")
-
-
-#
-# check that file is in Unix format
-#
-def check_file_unix(line, len):
-
-    if len < 1:
-        return
-
-    if line[-1] == '\r':
-        error("not in Unix format");
+        # check that else statements do not have preceeding
+        # end-bracket on the same line
+        if re.search('\s*\}\s*else', line):
+            self.error("else: ending if bracket should be on previous line")
 
 
-#
-# check for post-increment/decrement
-#
-def check_pre_incr(line, len):
+    #
+    # check that file is in Unix format
+    #
+    def check_file_unix(self, line, len):
 
-    m = re.search('(^\s+\w+[+-]{2};)', line)
-    if m:
-        op = m.group(1)
-        if op.find('++') != -1:
-            error("Use pre-increment: %s" % op);
-        else:
-            error("Use pre-decrement: %s" % op);
+        if len < 1:
+            return
+
+        if line[-1] == '\r':
+            self.error("not in Unix format");
 
 
-"""
-Function that does a simplified recursive globbing,
-and returns a list of matches.
-"""
-def rec_quasiglob(top, patterns):
-    for root, dirs, files in os.walk(top, topdown=False):
-        for file in files:
-            for pattern in patterns:
-                if fnmatch.fnmatch(file, pattern):
-                    path = os.path.join(root, file)
-                    parse_any_file(path)
+    #
+    # check for post-increment/decrement
+    #
+    def check_pre_incr(self, line, len):
+
+        m = re.search('(^\s+\w+[+-]{2};)', line)
+        if m:
+            op = m.group(1)
+            if op.find('++') != -1:
+                self.error("Use pre-increment: %s" % op);
+            else:
+                self.error("Use pre-decrement: %s" % op);
 
 
-#
-# scan all files of extensions .xyz
-# parse all files
-#
-def build_file_list(top):
-    for e in extensions:
-        em = extmap[e]
-        rec_quasiglob(top, em)
+    def process_line(self, line, funcs, ext):
+
+        line = line.rstrip('\n')
+        line_len = len(line)
+
+        for func in self.common_checks:
+            func(line, line_len)
+
+        for func in funcs:
+            func(line, line_len)
+
+        if ext in self.maxsize:
+            (x, y) = self.maxsize[ext];
+            self.check_xy_max(line, line_len, x)
 
 
-#
-# map of extensions, and which checks to perform
-#
+    def parse_file(self, filename, ext):
 
-
-common_checks = [check_whitespace, check_whitespace2,
-                 check_termination, check_hex_lowercase,
-                 check_pre_incr, check_file_unix]
-
-mapping = {
-    'c':    [check_brackets, check_c_preprocessor],
-    'cpp':  [check_brackets],
-    'h':    [check_brackets],
-    'mk':   [],
-    'm4':   [check_brackets, check_c_comments],
-    }
-
-
-def process_line(line, funcs, ext):
-
-    line = line.rstrip('\n')
-    line_len = len(line)
-
-    for func in common_checks:
-        func(line, line_len)
-
-    for func in funcs:
-        func(line, line_len)
-
-    if ext in maxsize:
-        (x, y) = maxsize[ext];
-        check_xy_max(line, line_len, x)
+        funcs = self.funcmap[ext]
     
+        file = open(filename)
 
-def parse_file(filename, ext):
-#    print "parsing: %s (ext=%s)" % (filename, ext)
-
-    funcs = mapping[ext]
+        self.cur_filename = filename
     
-    file = open(filename)
-
-    global cur_filename, cur_lineno
-    cur_filename = filename
-    
-    while 1:
-        lines = file.readlines(100000)
-        if not lines:
-            break
-        cur_lineno = 0
-        for line in lines:
-            cur_lineno += 1
-            process_line(line, funcs, ext)
+        while 1:
+            lines = file.readlines(100000)
+            if not lines:
+                break
+            self.cur_lineno = 0
+            for line in lines:
+                self.cur_lineno += 1
+                self.process_line(line, funcs, ext)
 
 
-def parse_any_file(f):
-#    print "parse_any_file: " + f
-    for e in extensions:
-        em = extmap[e]
-        for m in em:
-            if fnmatch.fnmatch(f, m):
-                files[e].append(f)
-                parse_file(f, e)
-                return
-    print "unknown extension: " + f
+    def parse_any_file(self, f):
+        for e in self.extensions:
+            em = self.extmap[e]
+            for m in em:
+                if fnmatch.fnmatch(f, m):
+                    self.files[e].append(f)
+                    self.parse_file(f, e)
+                    return
+        print "unknown extension: " + f
 
 
-#
-# print usage
-#
+    def rec_quasiglob(self, top, patterns):
+        for root, dirs, files in os.walk(top, topdown=False):
+            for file in files:
+                for pattern in patterns:
+                    if fnmatch.fnmatch(file, pattern):
+                        path = os.path.join(root, file)
+                        self.parse_any_file(path)
+
+
+    def build_file_list(self, top):
+        for e in self.extensions:
+            em = self.extmap[e]
+            self.rec_quasiglob(top, em)
+
+
+###
+### END OF CLASS
+###
+
+
 def usage():
-    print "ccheck version " + version
+    print "ccheck version " + VERSION
     print ""
     print "Usage:"
     print ""
@@ -346,33 +319,30 @@ def usage():
 #
 
 
-# empty dict
-for e in extensions:
-    files[e] = []
-
-
 def main():
+    cc = ccheck()
+    
     if len(sys.argv) > 1:
         if sys.argv[1] == '--help':
             usage()
             exit(2)
 
-            for f in sys.argv[1:]:
-                print "checking: " + f
-                if os.path.isdir(f):
-                    build_file_list(f)
-                elif os.path.isfile(f):
-                    parse_any_file(f)
-                else:
-                    print "unknown file type: " + f
+        for f in sys.argv[1:]:
+            print "checking: " + f
+            if os.path.isdir(f):
+                cc.build_file_list(f)
+            elif os.path.isfile(f):
+                cc.parse_any_file(f)
+            else:
+                print "unknown file type: " + f
     else:
         # scan all files recursively
-        build_file_list('.')    
+        cc.build_file_list('.')    
 
     # done - print stats
-    print_stats()
+    cc.print_stats()
 
-    exit(errors != 0)
+    exit(cc.errors != 0)
 
 
 if __name__ == "__main__":
