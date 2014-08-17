6526a6e9b66e79c3ac75d531917f6e3cc464739b
Alfred E. Heggestad <aeh@db.org>
2014-08-17 21:06:38 +0200

    build.py -- add support for git, cleanups
    - add support for git projects using the [gits] config section
    - remove HTML status output
    - remove do_install option
    - remove 'svn_base', each project must now specify full SVN path

--- build.py
+++ build.py
@@ -2,7 +2,7 @@
 #
 # build.py Build script
 #
-# Copyright 2005 - 2010 Alfred E. Heggestad. All rights reserved.
+# Copyright 2005 - 2014 Alfred E. Heggestad. All rights reserved.
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License version 2 as
@@ -10,16 +10,18 @@
 #
 # TODO:
 #
-#  - multiple (cross) compilers
 #  - debian build gives some warnings/errors
-#  - html-table output
+#
+#
+# normal messages are written to STDOUT
+# error messages are written to STDERR
 #
 
 import os, sys, platform, getpass, subprocess, shutil, time, re
 import ConfigParser
 
 # constants
-VERSION  = '0.8.0'
+VERSION  = '0.8.1'
 LOGEXT   = 'txt'
 CCHECK   = '/usr/local/bin/ccheck.py'
 HOSTNAME = platform.node()
@@ -37,8 +39,8 @@ def linecount(fname):
 class Build:
     # default flags
     do_svn = 1
+    do_git = 1
     do_build = 1
-    do_install = 0
     do_deb = 0
     do_rpm = 0
     do_ccheck = 1
@@ -49,8 +51,8 @@ class Build:
         print "Build init: %s" % root_dir
 
         self.do_svn     = config.getboolean('tests', 'do_svn')
+        self.do_git     = config.getboolean('tests', 'do_git')
         self.do_build   = config.getboolean('tests', 'do_build')
-        self.do_install = config.getboolean('tests', 'do_install')
         self.do_deb     = config.getboolean('tests', 'do_deb')
         self.do_rpm     = config.getboolean('tests', 'do_rpm')
         self.do_ccheck  = config.getboolean('tests', 'do_ccheck')
@@ -60,6 +62,7 @@ class Build:
         self.make = config.get('core', 'make')
         self.cc = config.get('core', 'cc')
         self.svn = config.get('core', 'svn')
+        self.git = config.get('core', 'git')
 
         self.cc_ver = subprocess.Popen([self.cc, "--version"], \
                                        stdout=subprocess.PIPE).\
@@ -67,12 +70,12 @@ class Build:
 
         self.root_dir = root_dir
         self.log_dir = os.path.join(self.root_dir, 'log')
-        self.svn_dir = os.path.join(self.root_dir, 'svn')
+        self.src_dir = os.path.join(self.root_dir, 'src')
 
         self.clean_dir(self.log_dir)
 
-        if self.do_svn:
-            self.clean_dir(self.svn_dir)
+        if self.do_svn or self.do_git:
+            self.clean_dir(self.src_dir)
 
 
     def clean_dir(self, dir):
@@ -82,12 +85,12 @@ class Build:
             os.makedirs(dir)
 
 
-    def logfile(self, prefix, module, branch):
-        f = prefix + '-' + module + '-' + branch + '.' + LOGEXT
+    def logfile(self, prefix, module):
+        f = prefix + '-' + module + '.' + LOGEXT
         return os.path.join(self.log_dir, f)
 
 
-    def check_log(self, logfile, type, module, branch, pattern=None):
+    def check_log(self, logfile, type, module, pattern=None):
         if os.path.getsize(logfile):
             heading = False
             f = open(logfile, 'r')
@@ -105,7 +108,7 @@ class Build:
                         heading = True
                         print >> sys.stderr, '----- ' + type \
                               + ' failed for ' \
-                              + module + '/' + branch + ' -----'
+                              + module + ' -----'
                     sys.stderr.write(line)
 
             f.close()
@@ -124,256 +127,105 @@ class Build:
             subprocess.Popen(cmd, shell=True).communicate()
 
 
-    def svn_update(self, svn_base, module, branch):
-        print "subversion update [%s, %s]..." % (module, branch)
+    def svn_update(self, name, url):
+        print "subversion update [%s, %s]..." % (name, url)
 
-        if branch == 'trunk':
-            url = os.path.join(svn_base, module, 'trunk')
-        else:
-            url = os.path.join(svn_base, module, 'branches', branch)
+        path = os.path.join(self.src_dir, name)
 
-        path = os.path.join(self.svn_dir, branch, module)
-
-        lf = self.logfile('svn', module, branch)
+        lf = self.logfile('svn', name)
         self.run_op(path, self.svn + ' co ' + url + ' ' + path, lf)
-        self.check_log(lf, 'svn', module, branch, 'error:')
 
 
-    def run_ccheck(self, module, branch):
-        print "running ccheck [%s, %s]..." % (module, branch)
+    def git_clone(self, name, url):
+        print "git clone [%s, %s]..." % (name, url)
 
-        mod = os.path.join(self.svn_dir, branch, module)
-        lf = self.logfile('ccheck', module, branch)
+        path = os.path.join(self.src_dir, name)
+
+        lf = self.logfile('git', name)
+        self.run_op(path, self.git + ' clone ' + url + ' ' + path, lf)
+
+
+    def run_ccheck(self, module):
+        print "running ccheck [%s]..." % (module)
+
+        mod = os.path.join(self.src_dir, module)
+        lf = self.logfile('ccheck', module)
 
         cmd = 'cd ' + mod + '&&' + CCHECK + ' --quiet >>' + lf + ' 2>&1'
         subprocess.Popen(cmd, shell=True).communicate()
 
-        self.check_log(lf, 'ccheck', module, branch)
+        self.check_log(lf, 'ccheck', module)
 
 
-    def build_binaries(self, module, branch):
-        print "building binaries [%s, %s]..." % (module, branch)
+    def build_binaries(self, module):
+        print "building binaries [%s]..." % (module)
 
-        path = os.path.join(self.svn_dir, branch, module)
-        lf = self.logfile('binaries', module, branch)
+        path = os.path.join(self.src_dir, module)
+        lf = self.logfile('binaries', module)
 
         self.run_op(path, self.make + ' CC=' + self.cc, lf)
 
-        if self.do_install:
-            self.run_op(path, self.make + ' install CC=' + self.cc, lf)
-
-        self.check_log(lf, 'binaries', module, branch, 'warning|error[ :]')
+        self.check_log(lf, 'binaries', module, 'warning|error[ :]')
 
 
-    def run_splint(self, module, branch):
-        print "running splint [%s, %s]..." % (module, branch)
+    def run_splint(self, module):
+        print "running splint [%s]..." % (module)
 
-        lf = self.logfile('splint', module, branch)
+        lf = self.logfile('splint', module)
 
-        self.run_op(os.path.join(self.svn_dir, branch, module),
+        self.run_op(os.path.join(self.src_dir, module),
                     self.make + ' splint', lf)
 
-        self.check_log(lf, 'splint', module, branch)
+        self.check_log(lf, 'splint', module)
 
 
-    def run_doxygen(self, module, branch):
+    def run_doxygen(self, module):
 
-        path = os.path.join(self.svn_dir, branch, module)
-        lf = self.logfile('doxygen', module, branch)
+        path = os.path.join(self.src_dir, module)
+        lf = self.logfile('doxygen', module)
 
         if os.path.isfile(path + '/mk/Doxyfile'):
-            print "running doxygen [%s, %s]..." % (module, branch)
+            print "running doxygen [%s]..." % (module)
 
             self.run_op(path, self.make + ' dox', lf)
 
-            self.check_log(lf, 'doxygen', module, branch, 'warning |error ')
+            self.check_log(lf, 'doxygen', module, 'warning |error ')
 
 
     # Make Debian package
-    def make_deb(self, module, branch):
-        print "make deb [%s, %s]..." % (module, branch)
+    def make_deb(self, module):
+        print "make deb [%s]..." % (module)
 
-        path = os.path.join(self.svn_dir, branch, module)
-        lf = self.logfile('makedeb', module, branch)
+        path = os.path.join(self.src_dir, module)
+        lf = self.logfile('makedeb', module)
 
         if os.path.exists(path + '/debian'):
             self.run_op(path, self.make + ' deb', lf)
-            self.check_log(lf, 'makedeb', module, branch, 'warning|error[ :]')
+            self.check_log(lf, 'makedeb', module, 'warning|error[ :]')
 
 
     # Make RPM package
-    def make_rpm(self, module, branch):
-        print "make rpm [%s, %s]..." % (module, branch)
+    def make_rpm(self, module):
+        print "make rpm [%s]..." % (module)
 
-        path = os.path.join(self.svn_dir, branch, module)
-        lf = self.logfile('makerpm', module, branch)
+        path = os.path.join(self.src_dir, module)
+        lf = self.logfile('makerpm', module)
 
         if os.path.exists(path + '/rpm'):
             self.run_op(path, self.make + ' rpm', lf)
 
-            self.check_log(lf, 'makerpm', module, branch, 'warning|error[ :]')
+            self.check_log(lf, 'makerpm', module, 'warning|error[ :]')
 
 
-    def run_tests(self, svn_base, mods):
+    def run_tests(self, mods):
         for mod in mods:
-            for b in mods[mod]:
-                if self.do_svn: self.svn_update(svn_base, mod, b)
-        for mod in mods:
-            for b in mods[mod]:
-                if self.do_ccheck:  self.run_ccheck(mod, b)
-                if self.do_splint:  self.run_splint(mod, b)
-                if self.do_doxygen: self.run_doxygen(mod, b)
-                if self.do_build:   self.build_binaries(mod, b)
-                if self.do_deb:     self.make_deb(mod, b)
-                if self.do_rpm:     self.make_rpm(mod, b)
+            if self.do_ccheck:  self.run_ccheck(mod)
+            if self.do_splint:  self.run_splint(mod)
+            if self.do_doxygen: self.run_doxygen(mod)
+            if self.do_deb:     self.make_deb(mod)
+            if self.do_rpm:     self.make_rpm(mod)
 
 
-    def parse_svn(self, logfile):
-        f = open(logfile, 'r')
-
-        added = rev = 0
-        for line in f:
-            m = re.search('Checked out revision ([0-9]+)', line)
-            if m:
-                rev = m.group(1)
-
-            if re.search('^A ', line, re.I):
-                added += 1
-
-        f.close()
-
-        return '' + str(added) + ' files, revision ' + str(rev)
-
-
-    def parse_log(self, logfile):
-        f = open(logfile, 'r')
-
-        loghtml = logfile.replace(self.root_dir, '', 1)
-
-        (lines, err, warn) = (0, 0, 0)
-
-        for line in f:
-            lines += 1
-            if re.search('error[ :]', line, re.I):
-                err += 1
-            if re.search('warning[ :]', line, re.I):
-                warn += 1
-
-        f.close()
-
-        if err > 0:
-            return '<font color=\"#ff6666\">[Failed]</font>' + \
-                   ' with ' + str(err) + ' error(s) and ' \
-                   + str(warn) + ' warning(s)' + \
-                   '(<a href=\"' + loghtml + '\">logs</a>)'
-        elif warn > 0:
-            return '<font color=\"#999900\">[Failed]</font>' + \
-                   ' with ' + str(warn) + ' warning(s)' + \
-                   '(<a href=\"' + loghtml + '\">logs</a>)'
-        elif lines == 0:
-            return '<font color=\"#990000\">Fatal</font> - '\
-                   + loghtml + ' is empty'
-        else:
-            return '<font color=\"#009900\">[Passed]</font>'
-
-
-    def parse_ccheck(self, logfile):
-        err = linecount(logfile)
-
-        if err > 0:
-            return '<font color=\"#ff6666\">[Failed]</font>' + \
-                   ' with ' + str(err) + ' error(s) ' + \
-                   '(<a href=\"log/' + logfile + '\">logs</a>)'
-        else:
-            return '<font color=\"#009900\">[Passed]</font>'
-
-
-    def gen_status(self, mods):
-        print "generating status page..."
-
-        htmlfile = os.path.join(self.root_dir, 'index.html')
-        title = "Daily builds"
-
-        f = open(htmlfile, 'w')
-
-        f.write('<html>\n')
-        f.write('<head>\n')
-        f.write('<title>' + title + '</title>\n')
-        f.write('<link rel=\"stylesheet\" type=\"text/css\" '\
-                'media=\"screen\" href=\"/css/std.css\" />\n')
-        f.write('</head>\n')
-
-        f.write('<body>\n')
-
-        for mod in mods:
-            for b in mods[mod]:
-
-                f.write('<b>' + mod + ', branch ' + b + '</b>\n')
-                f.write('<ul>\n')
-
-                if self.do_svn:
-                    lf = self.logfile('svn', mod, b)
-                    f.write('<li>svn: ' \
-                            + self.parse_svn(lf) + ' ' \
-                            + self.parse_log(lf) \
-                            + '</li>\n')
-
-                if self.do_ccheck:
-                    f.write('<li>ccheck: ' + \
-                            self.parse_ccheck(self.logfile('ccheck', mod, b)) \
-                            + '</li>\n')
-
-                if self.do_splint:
-                    f.write('<li>splint: ' + \
-                            self.parse_ccheck(self.logfile('splint', mod, b)) \
-                            + '</li>\n')
-
-                if self.do_build:
-                    f.write('<li>binaries: ' + \
-                            self.parse_log(self.logfile('binaries', mod, b)) \
-                            + '</li>\n')
-
-                if self.do_deb:
-                    lf = self.logfile('makedeb', mod, b)
-                    if os.path.exists(lf):
-                        f.write('<li>Debian: ' + \
-                                self.parse_log(
-                                    self.logfile('makedeb', mod, b)) \
-                                + '</li>\n')
-
-                if self.do_rpm:
-                    lf = self.logfile('makedeb', mod, b)
-                    if os.path.exists(lf):
-                        f.write('<li>RPM: ' \
-                                + self.parse_log(
-                                    self.logfile('makerpm', mod, b)) \
-                                + '</li>\n')
-
-                if self.do_doxygen:
-                    path = os.path.join(self.svn_dir, b, mod, 'mk/Doxyfile')
-                    if os.path.exists(path):
-                        f.write('<li>doxygen: ' \
-                                + self.parse_log(
-                                    self.logfile('doxygen', mod, b)))
-                        f.write('<a href=\"' + \
-                                'svn/' + b + '/' + mod + \
-                                '-dox/html/index.html\">(html)</a>\n')
-
-                f.write('</li>\n')
-                f.write('</ul>\n')
-
-
-        f.write('<hr>\n')
-        f.write('Info:<br>' + self.cc_ver + '<br>' + UNAME + '<br>\n')
-
-        f.write('<hr>\n')
-        f.write('<i>Generated ' + time.ctime() + ' by ' \
-                + USERNAME + '@' + HOSTNAME + ' using ' \
-                + sys.argv[0] + ' version ' + VERSION + '</i><br><br>\n')
-
-        f.write('</body></html>\n')
-
-        f.close()
 
 
 def usage():
@@ -405,7 +257,8 @@ if __name__ == '__main__':
     apps = {}
     libs = {}
     mods = {}
-
+    gits = {}
+    
     if len (sys.argv) <= 1:
         usage()
         exit(2)
@@ -419,20 +272,44 @@ if __name__ == '__main__':
     config.read(config_file)
 
     root_dir = config.get('core', 'root_dir')
-    svn_base = config.get('core', 'svn_base')
     apps     = read_mods(config, 'apps')
     libs     = read_mods(config, 'libs')
+    gits     = read_mods(config, 'gits')
 
     for a in apps:
         mods[a] = apps[a]
     for l in libs:
         mods[l] = libs[l]
+    for g in gits:
+        mods[g] = gits[g]
 
     bld = Build(root_dir, config)
 
-    bld.run_tests(svn_base, libs)
-    bld.run_tests(svn_base, apps)
+    if bld.do_svn:
+        print "checkout subversion projects ..."
+        for name in libs:
+            url = libs[name][0]
+            bld.svn_update(name, url)
+        for name in apps:
+            url = apps[name][0]
+            bld.svn_update(name, url)
 
-    bld.gen_status(mods)
+    if bld.do_git:
+        print "clone GIT projects ..."
+        for name in gits:
+            url = gits[name][0]
+            bld.git_clone(name, url)
 
-    print 'build complete. logs at <http://' + HOSTNAME + '/build/>'
+    # Build step: libs must be built first
+    if bld.do_build:
+        print "building all projects ..."
+        for l in libs:
+            bld.build_binaries(l)
+        for a in apps:
+            bld.build_binaries(a)
+        for g in gits:
+            bld.build_binaries(g)
+
+    bld.run_tests(mods)
+
+    print 'build complete.'
